04029b74a12888ede94647fe401917157b831b26
Alan Z <fallenstar.9.30@gmail.com>
2025-02-27 18:48:45 +0800

    model

--- model/model.py
+++ model/model.py
@@ -6,12 +6,12 @@ by: Z.
 from utils import *
 
 
-class ResBlock(nn.Module):
+class ResNetBlock(nn.Module):
     def __init__(self,
                  in_channels: int,
                  out_channels: int,
                  kernel_size: Tuple[int, int, int] = (3, 3, 3)):
-        super(ResBlock, self).__init__()
+        super(ResNetBlock, self).__init__()
         self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size)
         self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size)
 
@@ -21,7 +21,7 @@ class ResBlock(nn.Module):
 
         self.skip_connection = nn.Identity()
 
-    def forward(self, x: Tensor):
+    def forward(self, x: Tensor) -> Tensor:
         residual = self.skip_connection(x)
         x = self.relu(self.bn1(self.conv1(x)))
         x = self.bn2(self.conv2(x))
@@ -32,8 +32,31 @@ class ResBlock(nn.Module):
 class DNN(nn.Module):
     def __init__(self):
         super(DNN, self).__init__()
-        conv1 = nn.Conv3d(1, 256, (3, 3, 3))
-        bn = nn.BatchNorm3d(256)
-        relu = nn.ReLU()
+        self.conv = nn.Sequential(*[
+            nn.Conv3d(1, 256, (3, 3, 3)),
+            nn.BatchNorm3d(256),
+            nn.ReLU(inplace=True)
+        ])
+        self.resnet_blocks = nn.Sequential(*[ResNetBlock(256, 256) for _ in range(19)])
+
+        self.policy_head = nn.Sequential(*[
+            nn.Conv3d(256, 2, (1, 1, 1)),
+            nn.BatchNorm3d(2),
+            nn.Tanh()
+        ])
+        self.value_head = nn.Sequential(*[
+            nn.Conv3d(256, 2, (1, 1, 1)),
+            nn.BatchNorm3d(2),
+            nn.Softmax()
+        ])
+
+    def forward(self, x: Tensor) -> Tuple[Tensor, Tensor]:
+        x = self.conv(x)
+        x = self.resnet_blocks(x)
+        policy, value = self.policy_head(x), self.value_head(x)
+        return policy, value
 
 
+if __name__ == "__main__":
+    net = DNN()
+    print(net)
